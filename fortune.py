c266985df35f201cb90e725e2ff0ef98390f1545
Miu Kotani <158299429+mk-v3@users.noreply.github.com>
2024-05-10 23:03:00 +0900

    Add window by Tkinter

--- fortune.py
+++ fortune.py
@@ -1,13 +1,28 @@
 import random
+import tkinter
 
-#おみくじ運リスト
-fortune_lists = ["大吉", "中吉", "小吉", "吉", "末吉", "凶", "最凶"]
+def draw_fortune():
+    # おみくじ運リスト
+    fortune_lists = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '最凶']
+    # 各運勢の重みづけ
+    fortune_weights = [1, 2, 3, 4, 3, 2, 1]
+    #ランダムで1つ引く
+    result = random.choices(fortune_lists, k=1, weights=fortune_weights)[0]
+    #結果を出力する print("今日の運勢は" + result + "です。")
+    result_label.config(text="今日の運勢は" + result + "です。")
+    result_label.pack()
 
-#各運勢の重みづけ
-fortune_weights = [1, 2, 3, 4, 3, 2, 1]
+#ウィンドウ
+root = tkinter.Tk()
+root.title("Omikuji")
+root.geometry("200x200")
 
-#ランダムで1つ引く
-result = random.choices(fortune_lists, k=1, weights=fortune_weights)[0]
+#おみくじを引くボタン
+button = tkinter.Button(text="おみくじを引く", width=50, command=draw_fortune)
+button.pack()
 
-#結果を出力する
-print("今日の運勢は" + result + "です。")
+result_label = tkinter.Label(root, text="", font=("Helvetica", 14))
+result_label.pack(pady=10)
+
+#ウィンドウを表示
+root.mainloop()
